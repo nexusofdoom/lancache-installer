faa29e2492d9fc50010553e57aab219f70134adf
nexusofdoom <travus@discoverpc.net>
2018-04-03 16:35:46 -0500

    Update install-sniproxy.sh

--- install-sniproxy.sh
+++ install-sniproxy.sh
@@ -5,5 +5,5 @@ git clone https://github.com/dlundquist/sniproxy
 cd sniproxy/
 ./autogen.sh && dpkg-buildpackage
 #the file name might be something like sniproxy_x64_5335253.deb it would be up one directory.
-sudo dpkg -i ../*.deb
-sudo curl https://raw.githubusercontent.com/OpenSourceLAN/origin-docker/master/sniproxy/sniproxy.conf -o /etc/sniproxy.conf
+dpkg -i ../*.deb
+curl https://raw.githubusercontent.com/OpenSourceLAN/origin-docker/master/sniproxy/sniproxy.conf -o /etc/sniproxy.conf
