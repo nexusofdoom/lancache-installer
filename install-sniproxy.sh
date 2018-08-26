f97cd6ded731474a92d467d28eb87688340583dc
nexusofdoom <travus@discoverpc.net>
2018-08-26 12:49:56 -0500

    Update install-sniproxy.sh

--- install-sniproxy.sh
+++ install-sniproxy.sh
@@ -7,6 +7,11 @@ cd sniproxy/
 #the file name might be something like sniproxy_x64_5335253.deb it would be up one directory.
 dpkg -i ../*.deb
 curl https://raw.githubusercontent.com/OpenSourceLAN/origin-docker/master/sniproxy/sniproxy.conf -o /etc/sniproxy.conf
+##make direcotry needed
+mkdir -p /var/run/sniproxy
+mkdir -p /var/log/sniproxy
+adduser -d /var/empty -M -s /sbin/nologin -r sniproxy
+chown sniproxy:sniproxy /var/run/sniproxy
 echo ##########################################################################
 echo Please run install-lancache.sh
 echo ##########################################################################
