da8fea4c6165c6a783b189c1eae9765b929b453c
nexusofdoom <travus@discoverpc.net>
2018-08-30 14:42:02 -0500

    Update install-lancache.sh

--- install-lancache.sh
+++ install-lancache.sh
@@ -5,281 +5,143 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-## Set variables
+# Variables you should most likely not touch
+# Unless you know what you are doing
 lc_base_folder=/usr/local/lancache
-lc_git=/usr/local/temp
+lc_tmp_ip=/tmp/services_ips.txt
+lc_tmp_unbound=$lc_base_folder/temp/unbound.conf
+lc_tmp_hosts=$lc_base_folder/temp/hosts
 lc_nginx_loc=/etc/nginx
+#lc_sniproxy_bin=/usr/local/sbin/sniproxy
 lc_srv_loc=/srv/lancache
 lc_unbound_loc=/etc/unbound
-lc_date=$( date +"%m-%d-%y %T" )
-lc_hn=$( hostname )
-lc_int_log=interface_used.log
-lc_list_int=$( ls /sys/class/net )
-lc_ip_googledns1=8.8.8.8
-lc_ip_googledns2=8.8.4.4
-lc_ip_logfile=ip.log
-lc_ip_gw=$( /sbin/ip route | awk '/default/ { print $3 }' )
-apt-get install net-tools -y
-#rm old lancache files
-rm /usr/local/lancache -Rvf
-rm /usr/local/lancache -Rvf
-
-
-#Get Lancache Files from github keep track of the location where you download the files to
-git clone -b master http://github.com/nexusofdoom/lancache-installer "$lc_git"
-mv "$lc_git" "$lc_base_folder"
-## Create the necessary folders
-mkdir -p $lc_base_folder/config/
-mkdir -p $lc_base_folder/data/
-mkdir -p $lc_base_folder/logs/
-#Get Lancache Files
-chown -R $USER:$USER $lc_base_folder
-#--------tested so far april 2nd 2018
-
-
+#lc_nginx_url=http://nginx.org/download/nginx-$lc_nginx_version.tar.gz
+lc_tmp_yaml=$lc_base_folder/temp/01-lancache.yaml
+lc_dl_dir=/tmp/lancache
 ## Divide the ip in variables
-lc_ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
-lc_eth_int=$( ip route get 8.8.8.8 | awk '{print $5}' )
-lc_eth_netmask=$( ifconfig enp0s3 | grep inet | grep netmask | cut -f13 -d ' ' )
-lc_eth_net=$(ip -4 addr show enp0s3 | grep -oP "(?<=inet ).*(?=br)" )
+lc_ip_nginx=$( ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
+TIMESTAMP=$(date +%s)
+
+
+# Arrays used
+# Services used and set ip for and created the lancache folders for
+declare -a lc_services=(arena apple blizzard hirez gog glyph microsoft origin riot steam sony enmasse wargaming uplay zenimax digitalextremes pearlabyss)
+# Installer Folders
+declare -a lc_folders=(config data logs temp)
+# Log Folders
+declare -a lc_logfolders=(Access Error Keys)
+
+declare -a ip_eth=$( ip link show | grep ens | tr ":" " " | awk '{ print $2 }' )
+for int in ${ip_eth[@]}; do
+        inet_eth=$( ip route get 8.8.8.8 | tr " " " " | awk '{ print $5 }'  )
+        if [ "$inet_eth" == "$int" ]; then
+                lc_ip_eth=$int
+        fi
+done
+
+lc_ip=$( /bin/ip -4 addr show $lc_ip_eth | grep -oP "(?<=inet ).*(?=br)" )
+#1st octet
 lc_ip_p1=$(echo ${lc_ip} | tr "." " " | awk '{ print $1 }')
+#2nd octet
 lc_ip_p2=$(echo ${lc_ip} | tr "." " " | awk '{ print $2 }')
+#3rd octet
 lc_ip_p3=$(echo ${lc_ip} | tr "." " " | awk '{ print $3 }')
-lc_ip_p4=$(echo ${lc_ip} | tr "." " " | awk '{ print $4 }')
-
-## Increment the last IP digit for every Game
-lc_incr_steam=$((lc_ip_p4+1))
-lc_ip_steam=$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_incr_steam
-
-lc_incr_riot=$((lc_ip_p4+2))
-lc_ip_riot=$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_incr_riot
-
-lc_incr_blizzard=$((lc_ip_p4+3))
-lc_ip_blizzard=$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_incr_blizzard
-
-lc_incr_hirez=$((lc_ip_p4+4))
-lc_ip_hirez=$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_incr_hirez
-
-lc_incr_origin=$((lc_ip_p4+5))
-lc_ip_origin=$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_incr_origin
-
-lc_incr_sony=$((lc_ip_p4+6))
-lc_ip_sony=$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_incr_sony
-
-lc_incr_microsoft=$((lc_ip_p4+7))
-lc_ip_microsoft=$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_incr_microsoft
-
-lc_incr_enmasse=$((lc_ip_p4+8))
-lc_ip_enmasse=$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_incr_enmasse
-
-lc_incr_gog=$((lc_ip_p4+9))
-lc_ip_gog=$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_incr_gog
-
-lc_incr_arena=$((lc_ip_p4+10))
-lc_ip_arena=$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_incr_arena
-
-lc_incr_wargaming=$((lc_ip_p4+11))
-lc_ip_wargaming=$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_incr_wargaming
-
-lc_incr_uplay=$((lc_ip_p4+12))
-lc_ip_uplay=$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_incr_uplay
-
-lc_incr_apple=$((lc_ip_p4+13))
-lc_ip_apple=$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_incr_apple
-
-lc_incr_glyph=$((lc_ip_p4+14))
-lc_ip_glyph=$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_incr_glyph
-
-lc_incr_zenimax=$((lc_ip_p4+15))
-lc_ip_zenimax=$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_incr_zenimax
-
-lc_incr_digitalextremes=$((lc_ip_p4+16))
-lc_ip_digitalextremes=$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_incr_digitalextremes
-
-lc_incr_pearlabyss=$((lc_ip_p4+17))
-lc_ip_pearlabyss=$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_incr_pearlabyss
-
-## Put IP's in the log file
-echo [ lc_date ] Information !!! >>$lc_base_folder/logs/$lc_ip_logfile
-echo IP addresses being used: >>$lc_base_folder/logs/$lc_ip_logfile
-echo >>$lc_base_folder/logs/$lc_ip_logfile
-echo IP for $lc_eth_int is $lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_ip_p4 >>$lc_base_folder/logs/$lc_ip_logfile
-echo Netmask for $lc_eth_int is $lc_eth_netmask >>$lc_base_folder/logs/$lc_ip_logfile
-echo >>$lc_base_folder/logs/$lc_ip_logfile
-echo Arena: $lc_ip_arena >>$lc_base_folder/logs/$lc_ip_logfile
-echo Apple: $lc_ip_apple >>$lc_base_folder/logs/$lc_ip_logfile
-echo Blizzard: $lc_ip_blizzard >>$lc_base_folder/logs/$lc_ip_logfile
-echo GOG: $lc_ip_glyph >>$lc_base_folder/logs/$lc_ip_logfile
-echo GOG: $lc_ip_gog >>$lc_base_folder/logs/$lc_ip_logfile
-echo Hirez: $lc_ip_hirez >>$lc_base_folder/logs/$lc_ip_logfile
-echo Microsoft: $lc_ip_microsoft >>$lc_base_folder/logs/$lc_ip_logfile
-echo Origin: $lc_ip_origin >>$lc_base_folder/logs/$lc_ip_logfile
-echo Riot: $lc_ip_riot >>$lc_base_folder/logs/$lc_ip_logfile
-echo Steam: $lc_ip_steam >>$lc_base_folder/logs/$lc_ip_logfile
-echo Sony: $lc_ip_sony >>$lc_base_folder/logs/$lc_ip_logfile
-echo Enmasse: $lc_ip_enmasse >>$lc_base_folder/logs/$lc_ip_logfile
-echo Uplay: $lc_ip_uplay >>$lc_base_folder/logs/$lc_ip_logfile
-echo Wargaming: $lc_ip_wargaming >>$lc_base_folder/logs/$lc_ip_logfile
-echo Zenimax: $lc_ip_zenimax >>$lc_base_folder/logs/$lc_ip_logfile
-echo Digitalextremes: $lc_ip_digitalextremes >>$lc_base_folder/logs/$lc_ip_logfile
-echo Pearlabyss: $lc_ip_pearlabyss >>$lc_base_folder/logs/$lc_ip_logfile
-
-#unbound setup
-## Preparing configuration for unbound
-#mkdir -p /$lc_base_folder/unbound/
-sed -i 's|lc-host-ip|'$lc_ip'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-proxybind|'$lc_ip'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-gw|'$lc_ip_gw'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-arena|'$lc_ip_arena'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-apple|'$lc_ip_apple'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-blizzard|'$lc_ip_blizzard'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-hirez|'$lc_ip_hirez'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-gog|'$lc_ip_gog'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-glyph|'$lc_ip_glyph'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-microsoft|'$lc_ip_microsoft'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-origin|'$lc_ip_origin'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-riot|'$lc_ip_riot'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-steam|'$lc_ip_steam'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-sony|'$lc_ip_sony'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-enmasse|'$lc_ip_enmasse'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-wargaming|'$lc_ip_wargaming'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-uplay|'$lc_ip_uplay'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-zenimax|'$lc_ip_zenimax'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-digitalextremes|'$lc_ip_digitalextremes'|g' $lc_base_folder/etc/unbound/unbound.conf
-sed -i 's|lc-host-pearlabyss|'$lc_ip_pearlabyss'|g' $lc_base_folder/etc/unbound/unbound.conf
-
-#copy config for unbound into folder
-cp $lc_base_folder/etc/unbound/unbound.conf /etc/unbound/unbound.conf
-
-#Replace the interfaces: section with the normal ip (not the virtual ones)
-#Replace all "A records" with the appropriate IPs (the virtual IPs for the appropriate caching service like in hosts file)
-
-## Make the Necessary Changes For The New Host File
-sed -i 's|lc-hostname|'$lc_hn'|g' $lc_base_folder/etc/hosts
-sed -i 's|lc-host-proxybind|'$lc_ip'|g' $lc_base_folder/etc/hosts
-sed -i 's|lc-host-arena|'$lc_ip_arena'|g' $lc_base_folder/etc/hosts
-sed -i 's|lc-host-apple|'$lc_ip_apple'|g' $lc_base_folder/etc/hosts
-sed -i 's|lc-host-blizzard|'$lc_ip_blizzard'|g' $lc_base_folder/etc/hosts
-sed -i 's|lc-host-hirez|'$lc_ip_hirez'|g' $lc_base_folder/etc/hosts
-sed -i 's|lc-host-glyph|'$lc_ip_glyph'|g' $lc_base_folder/etc/hosts
-sed -i 's|lc-host-gog|'$lc_ip_gog'|g' $lc_base_folder/etc/hosts
-sed -i 's|lc-host-microsoft|'$lc_ip_microsoft'|g' $lc_base_folder/etc/hosts
-sed -i 's|lc-host-origin|'$lc_ip_origin'|g' $lc_base_folder/etc/hosts
-sed -i 's|lc-host-riot|'$lc_ip_riot'|g' $lc_base_folder/etc/hosts
-sed -i 's|lc-host-steam|'$lc_ip_steam'|g' $lc_base_folder/etc/hosts
-sed -i 's|lc-host-sony|'$lc_ip_sony'|g' $lc_base_folder/etc/hosts
-sed -i 's|lc-host-enmasse|'$lc_ip_enmasse'|g' $lc_base_folder/etc/hosts
-sed -i 's|lc-host-uplay|'$lc_ip_uplay'|g' $lc_base_folder/etc/hosts
-sed -i 's|lc-host-wargaming|'$lc_ip_wargaming'|g' $lc_base_folder/etc/hosts
-sed -i 's|lc-host-zenimax|'$lc_ip_zenimax'|g' $lc_base_folder/etc/hosts
-sed -i 's|lc-host-digitalextremes|'$lc_ip_digitalextremes'|g' $lc_base_folder/etc/hosts
-sed -i 's|lc-host-pearlabyss|'$lc_ip_pearlabyss'|g' $lc_base_folder/etc/hosts
-
-## Make the Necessary Changes For The New Interfaces File
-sed -i 's|lc-host-ip|'$lc_ip'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i "s^lc-host-network^$lc_eth_net^g" $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-gateway|'$lc_ip_gw'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-arena|'$lc_ip_arena'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-apple|'$lc_ip_apple'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-blizzard|'$lc_ip_blizzard'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-hirez|'$lc_ip_hirez'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-gog|'$lc_ip_gog'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-glyph|'$lc_ip_glyph'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-microsoft|'$lc_ip_microsoft'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-origin|'$lc_ip_origin'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-riot|'$lc_ip_riot'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-steam|'$lc_ip_steam'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-sony|'$lc_ip_sony'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-enmasse|'$lc_ip_enmasse'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-uplay|'$lc_ip_uplay'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-wargaming|'$lc_ip_wargaming'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-zenimax|'$lc_ip_zenimax'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-digitalextremes|'$lc_ip_digitalextremes'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-pearlabyss|'$lc_ip_pearlabyss'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-netmask|'$lc_eth_netmask'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
-sed -i 's|lc-host-vint|'$lc_eth_int'|g' $lc_base_folder/etc/netplan/01-netcfg.yaml
+#4th octet
+lc_ip_p4=$(echo ${lc_ip} | tr "." " " | awk '{ print $4 }' | cut -f1 -d "/" )
+#Subnet
+lc_ip_sn=$(echo ${lc_ip} | sed 's:.*/::' )
 
 
-#Making Directorys for Data and Logs
-echo making srv directorys
-mkdir -p /srv/lancache/data/blizzard/
-mkdir -p /srv/lancache/data/microsoft/
-mkdir -p /srv/lancache/data/installs/
-mkdir -p /srv/lancache/data/other/
-mkdir -p /srv/lancache/data/tmp/
-mkdir -p /srv/lancache/data/hirez/
-mkdir -p /srv/lancache/data/origin/
-mkdir -p /srv/lancache/data/riot/
-mkdir -p /srv/lancache/data/gog/
-mkdir -p /srv/lancache/data/sony/
-mkdir -p /srv/lancache/data/steam/
-mkdir -p /srv/lancache/data/wargaming
-mkdir -p /srv/lancache/data/arenanetworks
-mkdir -p /srv/lancache/data/uplay
-mkdir -p /srv/lancache/data/glyph
-mkdir -p /srv/lancache/data/zenimax
-mkdir -p /srv/lancache/data/digitalextremes
-mkdir -p /srv/lancache/data/pearlabyss
-mkdir -p /srv/lancache/logs/Errors
-mkdir -p /srv/lancache/logs/Keys
-mkdir -p /srv/lancache/logs/Access
+########### Update lancache config folder from github########################################
+mkdir $lc_dl_dir
+cd $lc_dl_dir
+git clone -b master http://github.com/nexusofdoom/lancache-installer
 
-#Change Ownership of folders
+
+#if [ ! -f "$lc_tmp_ip" ]; then
+        for service in ${lc_services[@]}; do
+                        # Check if the folder exists if not creates it
+                        if [ ! -d "/tmp/data/$service" ]; then
+                                mkdir -p /tmp/data/$service
+                        fi
+
+                        # Increases the IP with Every Run
+                        lc_ip_p4=$(expr $lc_ip_p4 + 1)
+                        # Writes the IP to A File to use it later on as Array
+                        # This for Netplan later on
+                        echo $lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_ip_p4/$lc_ip_sn >> "$lc_tmp_ip"
+
+                        # This Changes the Unbound File with the correct IP Adresses
+                        sed -i 's|lc-host-'$service'|'$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_ip_p4'|g' $lc_tmp_unbound
+
+                        # This Corrects the Host File For The Gameservices
+                        sed -i 's|lc-host-'$service'|'$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_ip_p4'|g' $lc_tmp_hosts
+
+                        # This Corrects the Host File For The Netplan
+                        sed -i 's|lc-host-'$service'|'$lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_ip_p4'|g' $lc_tmp_yaml
+
+#                done
+#        else
+#                echo Sorry Something went wrong as the file $lc_tmp_ip already exists!
+#fi
+
+for logfolder in ${lc_logfolders[@]}; do
+        # Check if the folder exists if not creates it
+        if [ ! -d "$lc_base_folder/$folder" ]; then
+                mkdir -p $lc_base_folder/$logfolder
+        fi
+done
+
+
+#Disable IPv6
+echo "net.ipv6.conf.all.disable_ipv6=1" > /etc/sysctl.d/disable-ipv6.conf
+sysctl -p /etc/sysctl.d/disable-ipv6.conf
+echo IPv6 disabled
+
+
+### Change file limits
+        sudo mv /etc/security/limits.conf /etc/security/limits.conf.$TIMESTAMP.bak
+        #need to get the limits into the /etc/security/limits.conf  * soft nofile  65536 * hard nofile  65536
+        echo '* soft nofile  65536' >> /etc/security/limits.conf
+        echo '* hard nofile  65536' >> /etc/security/limits.conf
+
+# Change Ownership of folders
 chown -R www-data:www-data /srv/lancache
-#chmod -R 777 /srv/lancache
 
-#######
-#Copy the conf folder and contents (where you originally git cloned it to in step 3) to /usr/local/nginx/conf/
-#cp -R $lc_base_folder/etc/nginx /etc/
+#Install Nginx Server
+apt install nginx -y
 
-#######
 ## Change the Proxy Bind in Lancache Configs
-#sed -i 's|lc-host-proxybind|'$lc_ip'|g' $lc_nginx_loc/sites-available/*.conf
-
-## Moving Base Files to The Correct Locations
-if [ -f "$lc_base_folder/etc/hosts" ]; then
-	mv /etc/hosts /etc/hosts.bak
-	cp $lc_base_folder/etc/hosts /etc/hosts
-fi
-
-if [ -f "$lc_base_folder/etc/netplan/01-netcfg.yaml" ]; then
-	mv /etc/netplan/01-netcfg.yaml /etc/netplan/01-netcfg.yaml.bak
-	cp $lc_base_folder/etc/netplan/01-netcfg.yaml /etc/netplan/01-netcfg.yaml
-fi
+sed -i 's|lc-host-proxybind|'$lc_ip_nginx'|g' $lc_nginx_loc/etc/nginx/sites-available/*.conf
+## Doing the necessary changes for Lancache
+mv $lc_nginx_loc/nginx.conf $lc_nginx_loc/nginx.conf.$TIMESTAMP.bak
+cp $lc_dl_dir/etc/nginx/nginx.conf $lc_nginx_loc/nginx.conf
+mkdir -p $lc_nginx_loc/conf/lancache
+cp $lc_dl_dir/etc/nginx/lancache/* $lc_nginx_loc/lancache
+cp $lc_dl_dir/etc/nginx/sites-available/*.conf $lc_nginx_loc/sites-available/
 
 
-# Updating local DNS resolvers
-echo "nameserver $lc_ip_googledns1" > /etc/resolv.conf
-echo "nameserver $lc_ip_googledns2" >> /etc/resolv.conf
+# Check if a previous workdir contains sniproxy
+# If not download it and compile it again
+apt install sniproxy -y
 
-#IP address for services
-## Put IP's in the log file
-echo "##########################################################################"
-echo Main network interface information
-echo IP $lc_ip_p1.$lc_ip_p2.$lc_ip_p3.$lc_ip_p4 
-echo Netmask$lc_eth_netmask 
-echo "##########################################################################"
-echo IP address used for services.
-echo Arena: $lc_ip_arena 
-echo Apple: $lc_ip_apple 
-echo Blizzard: $lc_ip_blizzard 
-echo GOG: $lc_ip_glyph 
-echo GOG: $lc_ip_gog 
-echo Hirez: $lc_ip_hirez 
-echo Microsoft: $lc_ip_microsoft 
-echo Origin: $lc_ip_origin 
-echo Riot: $lc_ip_riot 
-echo Steam: $lc_ip_steam 
-echo Sony: $lc_ip_sony 
-echo Enmasse: $lc_ip_enmasse 
-echo Uplay: $lc_ip_uplay 
-echo Wargaming: $lc_ip_wargaming 
-echo Zenimax: $lc_ip_zenimax 
-echo Digitalextremes: $lc_ip_digitalextremes 
-echo Pearlabyss: $lc_ip_pearlabyss 
-echo "##########################################################################"
+##enable sniproxy and move configs
+mv /etc/default/sniproxy /etc/default/sniproxy.$TIMESTAMP.bak
+cp $lc_dl_dir/etc/default/sniproxy  /etc/default/sniproxy
+mv /etc/default/sniproxy /etc/default/sniproxy.$TIMESTAMP.bak
+cp $lc_dl_dir/etc/sniproxy.conf   /etc/sniproxy.conf
 
 
-echo "##########################################################################"
-echo Lancache Installed pleae reboot and check services ,nginx,unbound,sniproxy
-echo "##########################################################################"
+echo "##############################################################################################"
+echo Current interface name
+#ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}'
+if_name=$(ifconfig | grep flags | awk -F: '{print $1;}' | grep -Fvx -e lo)
+echo "$if_name"
+
+### To Do Still
+### Change the proxy bind
+### Systemd Scripts for everything
+### ... and stuff I forgot ...
+### I have no problem with you redistributing this under your own name
+### Just leave the following piece of line in there
+### Base created by Geoffrey "bn_" @ https://github.com/bntjah
