3a9f0d48a42ebc0e51e8f4065a62174124514113
nexusofdoom <travus@discoverpc.net>
2018-09-01 11:08:42 -0500

    Update install-lancache.sh

--- install-lancache.sh
+++ install-lancache.sh
@@ -167,14 +167,14 @@ echo "$if_name"
 ### I have no problem with you redistributing this under your own name
 ### Just leave the following piece of line in there
 ### Base created by Geoffrey "bn_" @ https://github.com/bntjah
-echo $lc_base_folder
+#echo $lc_base_folder
 echo "##############################################################################################"
-echo DNS Server IP / Point Clients to Use this DNS Server.
+echo DNS Server IP Address / Point Clients to Use this DNS Server.
 echo $lc_network
-echo "##############################################################################################"
-echo $lc_base_folder
-echo $lc_ip_p4
-echo $lc_gateway
+#echo "##############################################################################################"
+#echo $lc_base_folder
+#echo $lc_ip_p4
+#echo $lc_gateway
 echo "##############################################################################################"
 echo Reboot System
 echo "##############################################################################################"
