06e8011bf32e6672919036d4f19f9c5b55da4961
nexusofdoom <travus@discoverpc.net>
2023-07-07 17:06:18 -0500

    Update install-lancache.sh

--- install-lancache.sh
+++ install-lancache.sh
@@ -63,7 +63,7 @@ apt -y install nginx sniproxy unbound nmon httpry netdata
 
 # Arrays used
 # Services used and set ip for and created the lancache folders for
-declare -a lc_services=(arena apple blizzard hirez gog glyph microsoft origin riot steam sony enmasse wargaming uplay zenimax digitalextremes pearlabyss epicgames)
+declare -a lc_services=(arenanetworks apple blizzard hirez gog glyph microsoft origin riot steam sony enmasse wargaming uplay zenimax digitalextremes pearlabyss epicgames)
 #declare -a lc_exclude_unbound=(steam)
 # Installer Folders
 declare -a lc_folders=(config data logs temp)
